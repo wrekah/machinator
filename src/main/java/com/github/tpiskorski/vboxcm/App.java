666e1b1dc46450e463633a0dbc22ec8befe6d461
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-01-06 05:30:26 +0100

    * added spring boot dep

--- src/main/java/com/github/tpiskorski/vboxcm/App.java
+++ src/main/java/com/github/tpiskorski/vboxcm/App.java
@@ -1,24 +1,51 @@
 package com.github.tpiskorski.vboxcm;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import java.io.IOException;
+import java.net.URL;
+
+@SpringBootApplication
 public class App extends javafx.application.Application {
+
+    private ConfigurableApplicationContext springContext;
+    private FXMLLoader fxmlLoader;
+    private Parent rootNode;
 
     public static void main(String[] args) {
         launch();
     }
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void stop() {
+        springContext.stop();
+    }
+
+    @Override
+    public void init() throws Exception {
+        springContext = SpringApplication.run(App.class);
+        fxmlLoader = new FXMLLoader();
+        URL resource = getClass().getResource("/fmxl/sample.fxml");
+        System.out.println(resource.getFile());
+        fxmlLoader.setLocation(resource);
+        fxmlLoader.setControllerFactory(springContext::getBean);
+    }
+    @Override
+    public void start(Stage stage) throws IOException {
+        rootNode = fxmlLoader.load();
+
+        stage.setTitle("Hello World");
+        Scene scene = new Scene(rootNode);
         stage.setScene(scene);
-        stage.show();
+        stage.show();;
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,12 @@
 module vboxcm {
     requires javafx.graphics;
     requires javafx.controls;
+    requires javafx.fxml;
+    requires spring.context;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+
+
     exports com.github.tpiskorski.vboxcm;
+    opens com.github.tpiskorski.vboxcm to spring.core;
 }
