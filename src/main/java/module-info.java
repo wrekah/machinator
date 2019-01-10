d5dad3398779d55a6958f776a1f26c8060c77410
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-01-11 00:01:12 +0100

    * added slf4j+log4j2 logging with spring boot starter * added config managment framework * adjusted assembled structure to  include log4j2 and config yml files * toggled off spring boot banner

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,9 @@ module vboxcm {
     requires spring.context;
     requires spring.boot;
     requires spring.boot.autoconfigure;
+    requires org.apache.logging.log4j;
+    requires slf4j.api;
+    requires cfg4j.core;
 
 
     exports com.github.tpiskorski.vboxcm;
