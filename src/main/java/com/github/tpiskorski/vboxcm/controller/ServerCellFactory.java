1564ce05598e647d328fbb20d7921dc04732db78
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-02-22 00:11:58 +0100

    * added listview icons to indicate whether the server is reachable or not

--- src/main/java/com/github/tpiskorski/vboxcm/controller/ServerCellFactory.java
+++ src/main/java/com/github/tpiskorski/vboxcm/controller/ServerCellFactory.java
@@ -3,9 +3,16 @@ package com.github.tpiskorski.vboxcm.controller;
 import com.github.tpiskorski.vboxcm.domain.Server;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.util.Callback;
 
 public class ServerCellFactory implements Callback<ListView<Server>, ListCell<Server>> {
+
+
+    private final Image OK = new Image("/icon/success.png");
+    private final Image NOT_OK = new Image("/icon/error.png");
+
     @Override
     public ListCell<Server> call(ListView<Server> param) {
         return new ListCell<>() {
@@ -13,19 +20,18 @@ public class ServerCellFactory implements Callback<ListView<Server>, ListCell<Se
             @Override
             protected void updateItem(Server server, boolean bln) {
                 super.updateItem(server, bln);
-
-                if (bln || server == null || server.getAddress() == null){
+                ImageView imageView = new ImageView();
+                if (bln || server == null || server.getAddress() == null) {
                     setText(null);
-                }else{
-                    if(server.isReachable().get()){
-                        this.setStyle("-fx-background-color: green;");
-
-                    }else{
-                        this. setStyle("-fx-background-color: red;");
+                } else {
+                    if (server.isReachable().get()) {
+                        imageView.setImage(OK);
+                    } else {
+                        imageView.setImage(NOT_OK);
                     }
 
+                    setGraphic(imageView);
                     setText(server.getAddress().get());
-
                 }
 
             }
