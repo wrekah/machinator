4d046a028833883a5cbc6d531d13d0b2cb140b82
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-02-22 02:23:19 +0100

    * adjusted vm list to requirements as well as modeled vm after it

--- src/main/java/com/github/tpiskorski/vboxcm/domain/VirtualMachine.java
+++ src/main/java/com/github/tpiskorski/vboxcm/domain/VirtualMachine.java
@@ -1,4 +1,96 @@
 package com.github.tpiskorski.vboxcm.domain;
 
+import javafx.beans.Observable;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.util.Callback;
+
+import java.util.Objects;
+
 public class VirtualMachine {
+
+   private StringProperty vmName = new SimpleStringProperty();
+   private StringProperty state = new SimpleStringProperty();
+   private StringProperty server = new SimpleStringProperty();
+   private IntegerProperty cpuCores = new SimpleIntegerProperty();
+   private IntegerProperty ramMemory = new SimpleIntegerProperty();
+
+   static Callback<VirtualMachine, Observable[]> extractor() {
+      return (VirtualMachine vm) -> new Observable[]{vm.vmNameProperty(), vm.stateProperty(), vm.serverProperty(), vm.cpuCoresProperty(), vm.ramMemoryProperty()};
+   }
+
+   public String getVmName() {
+      return vmName.get();
+   }
+
+   public void setVmName(String vmName) {
+      this.vmName.set(vmName);
+   }
+
+   public StringProperty vmNameProperty() {
+      return vmName;
+   }
+
+   public String getState() {
+      return state.get();
+   }
+
+   public void setState(String state) {
+      this.state.set(state);
+   }
+
+   public StringProperty stateProperty() {
+      return state;
+   }
+
+   public String getServer() {
+      return server.get();
+   }
+
+   public void setServer(String server) {
+      this.server.set(server);
+   }
+
+   public StringProperty serverProperty() {
+      return server;
+   }
+
+   public int getCpuCores() {
+      return cpuCores.get();
+   }
+
+   public void setCpuCores(int cpuCores) {
+      this.cpuCores.set(cpuCores);
+   }
+
+   public IntegerProperty cpuCoresProperty() {
+      return cpuCores;
+   }
+
+   public int getRamMemory() {
+      return ramMemory.get();
+   }
+
+   public void setRamMemory(int ramMemory) {
+      this.ramMemory.set(ramMemory);
+   }
+
+   public IntegerProperty ramMemoryProperty() {
+      return ramMemory;
+   }
+
+   @Override public int hashCode() {
+      return Objects.hash(server, vmName);
+   }
+
+   @Override public boolean equals(Object obj) {
+      if (!(obj instanceof VirtualMachine)) {
+         return false;
+      }
+      VirtualMachine that = (VirtualMachine) obj;
+
+      return Objects.equals(this.getServer(), that.getVmName());
+   }
 }
---
+++ src/main/java/com/github/tpiskorski/vboxcm/domain/VirtualMachineRepository.java
@@ -0,0 +1,23 @@
+package com.github.tpiskorski.vboxcm.domain;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class VirtualMachineRepository {
+
+   private ObservableList<VirtualMachine> vmObservableList = FXCollections.observableArrayList(VirtualMachine.extractor());
+
+   public void add(VirtualMachine vm) {
+      vmObservableList.add(vm);
+   }
+
+   public ObservableList<VirtualMachine> getServersList() {
+      return vmObservableList;
+   }
+
+   public void remove(VirtualMachine vm) {
+      vmObservableList.remove(vm);
+   }
+}
