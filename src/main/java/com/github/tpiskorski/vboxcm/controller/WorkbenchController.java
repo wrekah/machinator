9ef090f41f770d5797c55907fdb3f8ed7f7a9709
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-02-22 02:50:19 +0100

    * plugged virutal machine list * stub generator generatores vm as well * bindings on virtual machine action buttons

--- src/main/java/com/github/tpiskorski/vboxcm/controller/WorkbenchController.java
+++ src/main/java/com/github/tpiskorski/vboxcm/controller/WorkbenchController.java
@@ -3,18 +3,14 @@ package com.github.tpiskorski.vboxcm.controller;
 import com.github.tpiskorski.vboxcm.domain.Server;
 import com.github.tpiskorski.vboxcm.domain.ServerRepository;
 import com.github.tpiskorski.vboxcm.domain.VirtualMachine;
+import com.github.tpiskorski.vboxcm.domain.VirtualMachineRepository;
 import javafx.beans.binding.Bindings;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
@@ -30,81 +26,89 @@ import java.io.IOException;
 @Controller
 public class WorkbenchController {
 
-    @FXML
-    private Button removeServerButton;
-    @Autowired private ConfigurableApplicationContext springContext;
+   @FXML private TableColumn vmNameColumn;
 
-    @FXML
-    private TableView<VirtualMachine> virtualMachines;
-    @FXML
-    private TextField filterField;
-    @FXML
-    private ListView<Server> serverList;
+   @Autowired private ConfigurableApplicationContext springContext;
+   @Autowired private ServerRepository serverRepository;
+   @Autowired private VirtualMachineRepository virtualMachineRepository;
 
-    @Autowired private ServerRepository serverRepository;
+   @FXML private Button removeServerButton;
 
-    private ObservableList<Server> observableList = FXCollections.observableArrayList();
+   @FXML private Button removeVmButton;
+   @FXML private Button resetVmButton;
+   @FXML private Button powerOffVmButton;
+   @FXML private Button turnOffVmButton;
+   @FXML private Button turnOnVmButton;
 
-    public void initialize() {
-        removeServerButton.disableProperty().bind(Bindings.isEmpty(serverList.getSelectionModel().getSelectedItems()));
-        serverList.addEventFilter(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-            if (mouseEvent.getButton() == MouseButton.SECONDARY) {
-                serverList.getSelectionModel().clearSelection();
+   @FXML private TableView<VirtualMachine> virtualMachines;
+   @FXML private TextField filterField;
+   @FXML private ListView<Server> serverList;
+
+   public void initialize() {
+
+
+      removeVmButton.disableProperty().bind(Bindings.isEmpty(virtualMachines.getSelectionModel().getSelectedItems()));
+      resetVmButton.disableProperty().bind(Bindings.isEmpty(virtualMachines.getSelectionModel().getSelectedItems()));
+      powerOffVmButton.disableProperty().bind(Bindings.isEmpty(virtualMachines.getSelectionModel().getSelectedItems()));
+      turnOffVmButton.disableProperty().bind(Bindings.isEmpty(virtualMachines.getSelectionModel().getSelectedItems()));
+      turnOnVmButton.disableProperty().bind(Bindings.isEmpty(virtualMachines.getSelectionModel().getSelectedItems()));
+
+      removeServerButton.disableProperty().bind(Bindings.isEmpty(serverList.getSelectionModel().getSelectedItems()));
+      serverList.addEventFilter(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+         if (mouseEvent.getButton() == MouseButton.SECONDARY) {
+            serverList.getSelectionModel().clearSelection();
+         }
+      });
+
+      serverList.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
+         if (keyEvent.getCode() == KeyCode.ESCAPE) {
+            serverList.getSelectionModel().clearSelection();
+         }
+      });
+
+      FilteredList<Server> filteredData = new FilteredList<>(serverRepository.getServersList(), p -> true);
+
+      serverList.setCellFactory(new ServerCellFactory());
+      filterField.textProperty().addListener((observable, oldValue, newValue) -> {
+         filteredData.setPredicate(person -> {
+            // If filter text is empty, display all persons.
+            if (newValue == null || newValue.isEmpty()) {
+               return true;
             }
 
-        });
+            // Compare first name and last name of every person with filter text.
+            String lowerCaseFilter = newValue.toLowerCase();
 
-        serverList.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
-            if (keyEvent.getCode() == KeyCode.ESCAPE) {
-                serverList.getSelectionModel().clearSelection();
-            }
-        });
+            // Filter matches last name.
+            if (person.getAddress().get().toLowerCase().contains(lowerCaseFilter)) {
+               return true; // Filter matches first name.
+            } else return person.getAddress().get().toLowerCase().contains(lowerCaseFilter);
+         });
+      });
 
-        FilteredList<Server> filteredData = new FilteredList<>(serverRepository.getServersList(), p -> true);
-
-        serverList.setCellFactory(new ServerCellFactory());
-        filterField.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredData.setPredicate(person -> {
-                // If filter text is empty, display all persons.
-                if (newValue == null || newValue.isEmpty()) {
-                    return true;
-                }
-
-                // Compare first name and last name of every person with filter text.
-                String lowerCaseFilter = newValue.toLowerCase();
-
-                // Filter matches last name.
-                if (person.getAddress().get().toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches first name.
-                } else return person.getAddress().get().toLowerCase().contains(lowerCaseFilter);
-            });
-        });
-
-        serverList.setItems(filteredData);
-
-    }
+      serverList.setItems(filteredData);
 
 
-    public void removeServer() {
-        Server serverToRemove = serverList.getSelectionModel().getSelectedItem();
-        serverRepository.remove(serverToRemove);
+      virtualMachines.setItems(virtualMachineRepository.getServersList());
+   }
 
-    }
+   public void removeServer() {
+      Server serverToRemove = serverList.getSelectionModel().getSelectedItem();
+      serverRepository.remove(serverToRemove);
+   }
 
-    public void addServer() throws IOException {
-        Stage stage = new Stage();
-        stage.setResizable(false);
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        ClassPathResource mainFxml = new ClassPathResource("/fxml/addServer.fxml");
-        fxmlLoader.setControllerFactory(springContext::getBean);
-        fxmlLoader.setLocation(mainFxml.getURL());
-        Parent rootNode = fxmlLoader.load();
+   public void addServer() throws IOException {
+      Stage stage = new Stage();
+      stage.setResizable(false);
+      FXMLLoader fxmlLoader = new FXMLLoader();
+      ClassPathResource mainFxml = new ClassPathResource("/fxml/addServer.fxml");
+      fxmlLoader.setControllerFactory(springContext::getBean);
+      fxmlLoader.setLocation(mainFxml.getURL());
+      Parent rootNode = fxmlLoader.load();
 
-        Scene scene = new Scene(rootNode);
-        stage.setScene(scene);
+      Scene scene = new Scene(rootNode);
+      stage.setScene(scene);
 
-        stage.showAndWait();
-
-    }
-
+      stage.showAndWait();
+   }
 }
--- src/main/java/com/github/tpiskorski/vboxcm/stub/ServerListStubGenerator.java
+++ src/main/java/com/github/tpiskorski/vboxcm/stub/ServerListStubGenerator.java
@@ -3,6 +3,8 @@ package com.github.tpiskorski.vboxcm.stub;
 
 import com.github.tpiskorski.vboxcm.domain.Server;
 import com.github.tpiskorski.vboxcm.domain.ServerRepository;
+import com.github.tpiskorski.vboxcm.domain.VirtualMachine;
+import com.github.tpiskorski.vboxcm.domain.VirtualMachineRepository;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
@@ -16,13 +18,30 @@ import java.util.stream.IntStream;
 public class ServerListStubGenerator implements InitializingBean {
 
     private final ServerRepository serverRepository;
+    private final VirtualMachineRepository virtualMachineRepository;
 
-    @Autowired public ServerListStubGenerator(ServerRepository serverRepository) {
+    @Autowired public ServerListStubGenerator(ServerRepository serverRepository, VirtualMachineRepository virtualMachineRepository) {
         this.serverRepository = serverRepository;
+        this.virtualMachineRepository = virtualMachineRepository;
     }
 
     @Override public void afterPropertiesSet() {
         int serversInList = ThreadLocalRandom.current().nextInt(0, 10);
-        IntStream.range(0, serversInList).forEach(num -> serverRepository.add(new Server("localhost:" + 10 * num)));
+        IntStream.range(0, serversInList).forEach(num -> {
+            String address = "localhost:" + 10 * num;
+            serverRepository.add(new Server(address));
+            int virtualMachines = ThreadLocalRandom.current().nextInt(1, 3);
+            for (int i = 0; i < virtualMachines; i++) {
+                VirtualMachine vm = new VirtualMachine();
+                vm.setCpuCores(4);
+                vm.setServer(address);
+                vm.setState("ON");
+                vm.setVmName("vm@"+i);
+                vm.setRamMemory(1024);
+                virtualMachineRepository.add(vm);
+            }
+
+
+        });
     }
 }
