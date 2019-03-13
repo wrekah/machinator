92565ed93aecaf67379aac01fc57bb0fc70261a5
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-03-13 02:31:10 +0100

    * drafting, extracted converters to separate method, one prompt

--- src/main/java/com/github/tpiskorski/vboxcm/ui/controller/AddVmBackupController.java
+++ src/main/java/com/github/tpiskorski/vboxcm/ui/controller/AddVmBackupController.java
@@ -8,7 +8,6 @@ import com.github.tpiskorski.vboxcm.ui.core.ContextAwareSceneLoader;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -37,54 +36,18 @@ public class AddVmBackupController {
     @FXML
     public void initialize() throws IOException {
 
-
-    frequency.textProperty().addListener(new ChangeListener<String>() {
-                @Override
-                public void changed(ObservableValue<? extends String> observable, String oldValue,
-                                    String newValue) {
-                    if (!newValue.matches("\\d*")) {
-                        frequency.setText(newValue.replaceAll("[^\\d]", ""));
-                    }
-                }
-            });
-
-
-        serverComboBox.setConverter(new StringConverter<Server>() {
-
-            @Override public String toString(Server server) {
-                if (server == null) {
-                    return null;
-                } else {
-                    return server.getAddress();
-                }
-            }
-
-            @Override public Server fromString(String s) {
-                return null;
-            }
-        }
-        );
-
-        vmComboBox.setConverter(new StringConverter<VirtualMachine>() {
-            @Override public String toString(VirtualMachine virtualMachine) {
-                if (virtualMachine == null) {
-                    return null;
-                } else {
-                    return virtualMachine.getVmName();
-                }
-            }
-
-            @Override public VirtualMachine fromString(String s) {
-                return null;
+        frequency.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                frequency.setText(newValue.replaceAll("[^\\d]", ""));
             }
         });
 
+        setConverters();
 
-        serverComboBox.getSelectionModel().selectedItemProperty().addListener((observableValue, server, t1) ->{
-            if(t1!=null){
+        serverComboBox.getSelectionModel().selectedItemProperty().addListener((observableValue, server, t1) -> {
+            if (t1 != null) {
 
             }
-
         });
 
         vmComboBox.disableProperty().bind(serverComboBox.valueProperty().isNull());
@@ -93,7 +56,6 @@ public class AddVmBackupController {
             public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldValue, Boolean newValue) {
                 System.out.println(oldValue);
                 System.out.println(newValue);
-                vmComboBox.setPromptText("Pick VM");
             }
         });
 //        vmComboBox.disableProperty().addListener((observableValue, aBoolean, t1) -> {
@@ -110,14 +72,46 @@ public class AddVmBackupController {
         serverComboBox.getItems().addListener((ListChangeListener<Server>) change -> {
             serverComboBox.getSelectionModel().clearSelection();
         });
-       // vmComboBox.setItems(virtualMachineService.getVms());
+        vmComboBox.setItems(virtualMachineService.getVms());
+    }
+
+    private void setConverters() {
+        serverComboBox.setConverter(
+            new StringConverter<>() {
+
+                @Override public String toString(Server server) {
+                    if (server == null) {
+                        return null;
+                    } else {
+                        return server.getAddress();
+                    }
+                }
+
+                @Override public Server fromString(String s) {
+                    return null;
+                }
+            }
+        );
+
+        vmComboBox.setConverter(
+            new StringConverter<>() {
+                @Override public String toString(VirtualMachine virtualMachine) {
+                    if (virtualMachine == null) {
+                        return null;
+                    } else {
+                        return virtualMachine.getVmName();
+                    }
+                }
+
+                @Override public VirtualMachine fromString(String s) {
+                    return null;
+                }
+            });
     }
 
     @FXML
     public void close() {
-        // get a handle to the stage
         Stage stage = (Stage) cancelButton.getScene().getWindow();
-        // do what you have to do
         stage.close();
     }
 }
