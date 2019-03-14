5f202811c43f98fc4c0791705f81d625171fc0aa
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-03-14 22:54:12 +0100

    * adjsuted and expanded test coverage

--- src/test/groovy/com/github/tpiskorski/vboxcm/core/server/ServerServiceTest.groovy
+++ src/test/groovy/com/github/tpiskorski/vboxcm/core/server/ServerServiceTest.groovy
@@ -1,13 +1,19 @@
 package com.github.tpiskorski.vboxcm.core.server
 
+import com.github.tpiskorski.vboxcm.core.vm.VirtualMachine
+import com.github.tpiskorski.vboxcm.core.vm.VirtualMachineRepository
+import com.github.tpiskorski.vboxcm.core.vm.VirtualMachineService
+import com.github.tpiskorski.vboxcm.core.vm.VirtualMachineState
 import spock.lang.Specification
 import spock.lang.Subject
 
 class ServerServiceTest extends Specification {
 
     def serverRepository = new ServerRepository()
+    def virtualMachineRepository = new VirtualMachineRepository()
+    def virtualMachineService = new VirtualMachineService(virtualMachineRepository)
 
-    @Subject service = new ServerService(serverRepository)
+    @Subject service = new ServerService(serverRepository, virtualMachineService)
 
     def 'should get no servers if nothing was added'() {
         expect:
@@ -68,5 +74,64 @@ class ServerServiceTest extends Specification {
 
         then:
         service.getServers() == [server1]
+    }
+
+    def 'should update not_reachable state to the server and vms'() {
+        given:
+        def server1 = new Server('some:address')
+        def server2 = new Server('some:otheraddress')
+
+        def vm1 = new VirtualMachine('some:address', 'vm1')
+        def vm2 = new VirtualMachine('some:address', 'vm2')
+        def vm3 = new VirtualMachine('some:otheraddress', 'v1')
+
+        and:
+        service.add(server1)
+        service.add(server2)
+
+        and:
+        virtualMachineService.add(vm1)
+        virtualMachineService.add(vm2)
+        virtualMachineService.add(vm3)
+
+        when:
+        service.updateUnreachable(server1)
+
+        then:
+        server1.serverState == ServerState.NOT_REACHABLE
+        vm1.state == VirtualMachineState.UNREACHABLE
+        vm2.state == VirtualMachineState.UNREACHABLE
+    }
+
+    def 'should update reachable state to the server and vms'(){
+        given:
+        def server1 = new Server('some:address')
+        def server2 = new Server('some:otheraddress')
+
+        def vm1 = new VirtualMachine('some:address', 'vm1')
+        def vm2 = new VirtualMachine('some:address', 'vm2')
+        def vm3 = new VirtualMachine('some:otheraddress', 'v1')
+
+        and:
+        service.add(server1)
+        service.add(server2)
+
+        and:
+        virtualMachineService.add(vm1)
+        virtualMachineService.add(vm2)
+        virtualMachineService.add(vm3)
+
+        and:
+        def newVm1 = new VirtualMachine('some:address', 'some new vm')
+
+        when:
+        service.updateReachable(server1, [newVm1])
+
+        then:
+        server1.serverState == ServerState.REACHABLE
+
+        and:
+        virtualMachineService.getVms(server1).contains(newVm1)
+        virtualMachineService.getVms().contains(newVm1)
     }
 }
