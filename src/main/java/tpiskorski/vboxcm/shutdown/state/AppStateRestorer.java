7f8750d525b775c5dc10ca528967bc3c56eb79d7
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-04-05 02:08:40 +0200

    * app restorer doesnt trigger for dev

--- src/main/java/tpiskorski/vboxcm/shutdown/state/AppStateRestorer.java
+++ src/main/java/tpiskorski/vboxcm/shutdown/state/AppStateRestorer.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import tpiskorski.vboxcm.core.server.Server;
 import tpiskorski.vboxcm.core.server.ServerService;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Profile("!dev")
 @Service
 public class AppStateRestorer implements InitializingBean {
 
