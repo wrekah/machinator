7382f5781840f0ffacf97690f19461771e670a1a
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-04-11 23:06:51 +0200

    * extented test coverage and addressed scenario when job can be failed

--- src/test/groovy/tpiskorski/machinator/quartz/server/ServerRefreshJobListenerTest.groovy
+++ src/test/groovy/tpiskorski/machinator/quartz/server/ServerRefreshJobListenerTest.groovy
@@ -3,9 +3,12 @@ package tpiskorski.machinator.quartz.server
 import org.quartz.JobDetail
 import org.quartz.JobExecutionContext
 import org.quartz.JobExecutionException
+import org.quartz.JobKey
 import spock.lang.Specification
 import spock.lang.Subject
+import tpiskorski.machinator.core.job.Job
 import tpiskorski.machinator.core.job.JobService
+import tpiskorski.machinator.core.job.JobStatus
 
 class ServerRefreshJobListenerTest extends Specification {
 
@@ -31,5 +34,81 @@ class ServerRefreshJobListenerTest extends Specification {
         0 * jobService._
     }
 
+    def 'should create job before server refresh'() {
+        given:
+        def jobExecutionContext = Mock(JobExecutionContext) {
+            getJobDetail() >> Mock(JobDetail) {
+                getKey() >> JobKey.jobKey('someKey')
+                getJobClass() >> ServerRefreshJob.class
+            }
+        }
 
+        when:
+        listener.jobToBeExecuted(jobExecutionContext)
+
+        then:
+        1 * jobService.add(_) >> { assert (it[0].status == JobStatus.IN_PROGRESS) }
+    }
+
+    def 'should update job status when vetoed'() {
+        given:
+        def jobExecutionContext = Mock(JobExecutionContext) {
+            getJobDetail() >> Mock(JobDetail) {
+                getKey() >> JobKey.jobKey('someKey')
+                getJobClass() >> ServerRefreshJob.class
+            }
+        }
+
+        and:
+        def job = Mock(Job)
+
+        when:
+        listener.jobExecutionVetoed(jobExecutionContext)
+
+        then:
+        1 * jobService.getLastByDescription(_) >> job
+        1 * job.setStatus(JobStatus.CANCELLED)
+    }
+
+    def 'should update job status when executed without exceptions'() {
+        given:
+        def jobExecutionContext = Mock(JobExecutionContext) {
+            getJobDetail() >> Mock(JobDetail) {
+                getKey() >> JobKey.jobKey('someKey')
+                getJobClass() >> ServerRefreshJob.class
+            }
+        }
+
+        and:
+        def job = Mock(Job)
+
+        when:
+        listener.jobWasExecuted(jobExecutionContext, null)
+
+        then:
+        1 * jobService.getLastByDescription(_) >> job
+        1 * job.setStatus(JobStatus.COMPLETED)
+    }
+
+    def 'should update job status when failed to execute'() {
+
+        given:
+        def jobExecutionContext = Mock(JobExecutionContext) {
+            getJobDetail() >> Mock(JobDetail) {
+                getKey() >> JobKey.jobKey('someKey')
+                getJobClass() >> ServerRefreshJob.class
+            }
+        }
+        def jobExecutionException = Mock(JobExecutionException)
+
+        and:
+        def job = Mock(Job)
+
+        when:
+        listener.jobWasExecuted(jobExecutionContext, jobExecutionException)
+
+        then:
+        1 * jobService.getLastByDescription(_) >> job
+        1 * job.setStatus(JobStatus.FAILED)
+    }
 }
