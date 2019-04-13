3421b7ab52fb8cccf7bcbc174cae2ceb07651e67
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-04-14 00:08:14 +0200

    * escaping vm names as we can have white chars inside

--- src/main/java/tpiskorski/machinator/command/BaseCommand.java
+++ src/main/java/tpiskorski/machinator/command/BaseCommand.java
@@ -3,17 +3,17 @@ package tpiskorski.machinator.command;
 public enum BaseCommand {
     LIST_ALL_VMS("VBoxManage list vms"),
     IS_VBOX_INSTALLED("VBoxManage --version"),
-    SHOW_VM_INFO("VBoxManage showvminfo --machinereadable %s"),
-    START_VM("VBoxManage startvm --type headless %s"),
-    EXPORT_VM("VBoxManage export --output %s.ova %s");
-
-    public String asString() {
-        return baseCommand;
-    }
+    SHOW_VM_INFO("VBoxManage showvminfo --machinereadable \"%s\""),
+    START_VM("VBoxManage startvm --type headless \"%s\""),
+    EXPORT_VM("VBoxManage export --output %s.ova \"%s\"");
 
     private final String baseCommand;
 
     BaseCommand(String baseCommand) {
         this.baseCommand = baseCommand;
+    }
+
+    public String asString() {
+        return baseCommand;
     }
 }
