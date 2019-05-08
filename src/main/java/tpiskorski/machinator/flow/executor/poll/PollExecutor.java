ae1ca26b8c596a7ed2e985b507f441b21c75c61f
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-05-08 19:48:17 +0200

    * changed the way poll executor works, now it fails when the poll failed in the given tries

--- src/main/java/tpiskorski/machinator/flow/executor/poll/PollExecutor.java
+++ src/main/java/tpiskorski/machinator/flow/executor/poll/PollExecutor.java
@@ -1,18 +1,29 @@
 package tpiskorski.machinator.flow.executor.poll;
 
+import org.quartz.JobExecutionException;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class PollExecutor {
 
-    private static int POLL_TIMES = 10;
-    private static int POLL_INTERVAL_IN_SECONDS = 1;
+    private static final int MAX_TRIES = 10;
+    private static final int TRY_INTERVAL_IN_SECONDS = 1;
 
-    public void pollExecute(PollCommand pollCommand) throws InterruptedException, IOException {
-        int cycles = 0;
-        while (cycles < POLL_TIMES && !pollCommand.executeAndCheckIfSuccess()) {
-            TimeUnit.SECONDS.sleep(POLL_INTERVAL_IN_SECONDS);
-            cycles++;
+    public void pollExecute(PollCommand pollCommand) throws JobExecutionException {
+        try {
+            int tries = 0;
+            boolean result = false;
+            while (tries < MAX_TRIES && !result) {
+                result = pollCommand.executeAndCheckIfSuccess();
+                TimeUnit.SECONDS.sleep(TRY_INTERVAL_IN_SECONDS);
+                tries++;
+            }
+            if (!result) {
+                throw new JobExecutionException("Poll command failed after max tries");
+            }
+        } catch (InterruptedException | IOException e) {
+            throw new JobExecutionException(e);
         }
     }
 }
