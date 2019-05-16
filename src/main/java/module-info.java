6a6b80d4030b0f2a47269ba0cb1dc1741a70fa34
Tomasz Piskorski <tomasz.piskorski.x@gmail.com>
2019-05-17 01:36:40 +0200

    * controlsfx dependency for notifications

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,6 +14,7 @@ open module machinator {
     requires quartz;
     requires spring.context.support;
     requires jsch;
+    requires org.controlsfx.controls;
 
     exports tpiskorski.machinator;
 }
